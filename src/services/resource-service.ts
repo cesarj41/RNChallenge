702dfcc9d7ad20197401e9c38c9e25703ce7ac62
Cesar Martinez <cesarj4114@gmail.com>
2021-09-14 01:15:07 -0400

    added opening youtube video fallbback

--- src/services/resource-service.ts
+++ src/services/resource-service.ts
@@ -1,9 +1,17 @@
 import {Linking} from 'react-native';
 
 export const redirectAsync = async (uri: string) => {
+  const url = `vnd.youtube://watch?v=${uri}`;
+  const altUrl = `https://www.youtube.com/watch?v=${uri}`;
+
   try {
-    await Linking.openURL(`vnd.youtube://watch?v=${uri}`);
+    await Linking.openURL(url);
   } catch (error) {
-    console.error(error);
+    console.warn(error);
+    try {
+      await Linking.openURL(altUrl);
+    } catch (err) {
+      console.warn(err);
+    }
   }
 };
